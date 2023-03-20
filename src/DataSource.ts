3e62b5ca7e83b7eded702f56141fa1e3055cb92b
Maycon Lemos <mayconlemos.riosaude@gmail.com>
2023-03-20 11:13:05 -0300

    Update DataSource.ts

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
             type: "postgres",
-            host: "ec2-100-25-72-111.compute-1.amazonaws.com",
+            host: "ec2-hide.compute-1.amazonaws.com",
             port: 5432,
-            username: "dxgerfcrjrwiew",
-            password: "3ea47a9101db1ec89aeb25b26c58f082a3775199cbe3feb67bcce30c424d6f81",
-            database: "d6mu5pjnvhmt3t",
+            username: "hide",
+            password: "hide",
+            database: "hide",
             entities: ["build/database/entities/**/*.js"],
             synchronize: true,
             logging: false,
